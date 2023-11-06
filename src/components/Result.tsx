e9f62e867eaa139da61233fc296dec6e1ce738d9
romavsterg <romann.mikhaylov@gmail.com>
2023-11-06 11:20:36 +0300

    refactor: replaced states with context

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,68 +1,48 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import '../styles/Result.css';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
-import { book, details } from '../@types/types';
 import { Context } from '../Context/context';
+import { getBooks, getDetails } from '../utils/getBooks';
 
 const Result = () => {
-  console.log(useContext(Context));
+  const context = useContext(Context);
   const navigate = useNavigate();
-  const loacation = useLocation();
   const queryes = useLocation().search.match(/(?<=\w\=)\w*/g);
   const page = queryes ? Number(queryes[0]) : 1;
   const details = queryes ? queryes[1] : '';
   const search = useParams().query;
 
+  const lastPage = useRef<number | null>(null);
+  const lastDetails = useRef<string | null>(null);
+  const lastSearch = useRef<string | null | undefined>(null);
+
   const [areBooksLoading, setAreBooksLoading] = useState(false);
   const [areDetailsLoading, setAreDetailsLoading] = useState(false);
-  const [Books, setBooks] = useState<book[]>([]);
-  const [Details, setDetails] = useState<details>();
-
-  const getData = async (search = '*', page = 1) => {
-    const res = await axios.get(
-      `https://openlibrary.org/search.json?q=${decodeURI(
-        search
-      )}&limit=6&page=${page || 1}`
-    );
-    const data = res.data.docs;
-    return data;
-  };
 
   useEffect(() => {
-    const searchBooksAndDetails = async () => {
-      setAreBooksLoading(true);
-      const data = await getData(search, page);
-      const books: book[] = data.map((book: book) => {
-        return {
-          title: book.title,
-          author_name: book.author_name
-            ? book.author_name[0]
-            : `the author's name is not specified`,
-          key: book.key.replace(/\/works\//, ''),
-        };
-      });
-      setAreBooksLoading(false);
-      setBooks(books);
-      if (details) {
-        setAreDetailsLoading(true);
-        const data = await getData(details);
-        const filteredDetails = {
-          contributor: data[0].contributor
-            ? data[0].contributor[0]
-            : 'No specified contributors',
-          language: data[0].language
-            ? data[0].language[0]
-            : 'No language specified',
-          place: data[0].place ? data[0].place[0] : 'No place specified',
-        };
-
-        setDetails(filteredDetails);
-        setAreDetailsLoading(false);
-      }
-    };
-    searchBooksAndDetails();
-  }, [search, page, loacation, navigate, details]);
+    if (
+      page !== lastPage.current ||
+      details !== lastDetails.current ||
+      search !== lastSearch.current
+    ) {
+      const getData = async () => {
+        setAreBooksLoading(true);
+        const res = await getBooks(search, page);
+        context?.SetBooks(res);
+        setAreBooksLoading(false);
+        if (details) {
+          setAreDetailsLoading(true);
+          const res = await getDetails(details);
+          context?.SetDetails(res);
+          setAreDetailsLoading(false);
+        }
+      };
+      lastPage.current = page;
+      lastDetails.current = details;
+      lastSearch.current = search;
+      getData();
+    }
+  });
 
   const handleClick = () => {
     if (details) {
@@ -77,7 +57,7 @@ const Result = () => {
       ) : (
         <>
           <div className="books-cards" onClick={handleClick}>
-            {Books.map((book) => (
+            {context?.books.map((book) => (
               <Link
                 to={`/Components/search/${book.title.replace(
                   '/',
@@ -91,7 +71,7 @@ const Result = () => {
               </Link>
             ))}
           </div>
-          {details && (
+          {context?.details && (
             <div className="details">
               <button className="close-details" onClick={handleClick}>
                 Close details
@@ -100,9 +80,9 @@ const Result = () => {
                 <span className="loader"></span>
               ) : (
                 <>
-                  <h4>Contributor: {Details?.contributor}</h4>
-                  <h5>Language: {Details?.language}</h5>
-                  <h5>Place: {Details?.place}</h5>
+                  <h4>Contributor: {context?.details.contributor}</h4>
+                  <h5>Language: {context?.details.language}</h5>
+                  <h5>Place: {context?.details.place}</h5>
                 </>
               )}
             </div>
---
+++ src/utils/getBooks.ts
@@ -0,0 +1,40 @@
+import { book, details } from './../@types/types';
+import axios from 'axios';
+
+const getData = async (search = '*', page = 1) => {
+  const res = await axios.get(
+    `https://openlibrary.org/search.json?q=${decodeURI(search)}&limit=6&page=${
+      page || 1
+    }`
+  );
+  const data = res.data.docs;
+  return data;
+};
+
+export const getBooks = async (search = '*', page: number) => {
+  const data = await getData(search, page);
+  const books: book[] = data.map((book: book) => {
+    return {
+      title: book.title,
+      author_name: book.author_name
+        ? book.author_name[0]
+        : `the author's name is not specified`,
+      key: book.key.replace(/\/works\//, ''),
+    };
+  });
+
+  return books;
+};
+export const getDetails = async (search: string) => {
+  const data = await getData(search, 1);
+
+  const filteredDetails: details = {
+    contributor: data[0].contributor
+      ? data[0].contributor[0]
+      : 'No contributors specified',
+    language: data[0].language ? data[0].language[0] : 'No language specified',
+    place: data[0].place ? data[0].place[0] : 'No place specified',
+  };
+
+  return filteredDetails;
+};
