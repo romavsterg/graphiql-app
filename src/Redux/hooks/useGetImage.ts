4c3d35691cb4c6847edd6e60db666117e0eb8079
romavsterg <romann.mikhaylov@gmail.com>
2023-12-03 16:35:39 +0300

    feat: added displaing of image

--- src/Redux/hooks/useGetImage.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
-
-export const useGetImage = () => {
-  return useSelector((state: RootState) => state.image);
-};
